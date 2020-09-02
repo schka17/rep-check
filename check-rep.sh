5a858a26420fe7aa82770f10915e990e8aab3fe1
kschindler <kschindler@c4sam.com>
2020-09-02 15:53:28 +0200

    add download function

--- check-rep.sh
+++ check-rep.sh
@@ -18,7 +18,7 @@ cecho () {
  
     declare -A colors;
     colors=(\
-        ['black']='\E[0;47m'\
+        ['black']='\E[0;30m'\
         ['red']='\E[0;31m'\
         ['green']='\E[0;32m'\
         ['yellow']='\E[0;33m'\
@@ -28,14 +28,13 @@ cecho () {
         ['white']='\E[0;37m'\
         ['orange']='\E[0;33m'\
         ['purple']='\E[0;35m'\
-        ['lightGray']='\E[0;37m'\
-        ['darkGray']='\E[0;30m'\
-        ['lightGray']='\E[0;37m'\
-        ['lightRed']='\E[0;31m'\
-        ['lightGreen']='\E[0;32m'\
-        ['lightBlue']='\E[0;34m'\
-        ['lightPurple']='\E[0;35m'\
-        ['lightCyan']='\E[0;37m'\
+        ['BrightGray']='\E[0;90m'\
+        ['BrightRed']='\E[0;91m'\
+        ['BrightGreen']='\E[0;92m'\
+        ['BrightBlue']='\E[0;94m'\
+        ['BrightMagenta']='\E[0;95m'\
+        ['BrightCyan']='\E[0;96m'\
+        ['BrightWhite']='\E[0;97m'\
     );
  
     local defaultMSG="No message passed.";
@@ -87,12 +86,12 @@ error () {
  
 information () {
  
-    cecho -c 'lightBlue' "$@";
+    cecho -c 'BrightBlue' "$@";
 }
 
 success () {
  
-    cecho -c 'green' "$@";
+    cecho -c 'BrightGreen' "$@";
 }
 
 function GET_DEFAULT_INT {
@@ -142,7 +141,7 @@ function GET_DEFAULT_GW {
 _REQ_SW="true"
 _REQ_PORTS="true"
 _REQ_OS="debian"
-_REQ_OS_VER="10"
+_REQ_OS_VER="9"
 #_REPOSITORY="hal9000tng.homelinux.net"
 _REPOSITORY="gitlab.c4sam.com"
 _PORTS="22 443 6000"
@@ -157,29 +156,18 @@ _OS_NAME="$(grep "^NAME" /etc/*-release | cut -d"=" -f2-)"
 _DEFAULT_GW="$(netstat -rn | awk '/^0.0.0.0/ { print $2 }')"
 _FQDN="$(hostname -f)"
 #_DEFAULT_INT="$(route | awk '/default/ { print $8 }')"
-information "Hostname: ${_FQDN} Operating System: ${_OS} , Version: ${_OS_VER}, Default Interface: ${_DEFAULT_INT}, Default GW: ${_DEFAULT_GW}"
-# get public IP
-myWanIP=`dig +short myip.opendns.com @resolver1.opendns.com`
-#echo "Public IP is ${myWanIP}"
-information "Public IP is ${myWanIP}"
 
-# Checking for the resolved IP address from the end of the command output. Refer
-# the normal command output of nslookup to understand why.
-resolvedIP=$(nslookup "${_REPOSITORY}" | awk -F':' '/^Address: / { matched = 1 } matched { print $2}' | xargs)
 
-# Deciding the lookup status by checking the variable has a valid IP string
-[[ -z "$resolvedIP" ]] && error "${_REPOSITORY}" lookup failure || success "${_REPOSITORY} resolved to ${resolvedIP}"
-
-#traceroute $resolvedIP
-
-#ping -c1 -W1 -q $resolvedIP && echo "${resolvedIP} is reachable" || echo "${resolvedIP} is down"
-
+function PRINT_HASH {
+    cecho -c 'yellow' "############################################################################################"
+}
 
 function CHECK_REPOSITORY_ALIVE {
+    information "Check Connectivity to Host"
     ping -c1 -W1 -q $1 &>/dev/null
     status=$( echo $? )
     if [[ $status == 0 ]] ; then
-        success "$1 is reachable"
+        success "$1 is alive"
         #Connection success!
     else
         error "$1 is down"
@@ -210,7 +198,95 @@ function CHECK_PORT {
     fi
 }
 
+function INSTALL {
+    chmod 754 install_cockpit.sh &&./install_cockpit.sh && rm install_cockpit.sh
+}
+
+function DL_INSTALLER {
+    information "download installer using Token ${TOKEN}"
+    response=$(curl --request GET --header "PRIVATE-TOKEN: ${TOKEN}" -sL -w "%{http_code}" -o install_cockpit.sh 'https://gitlab.c4sam.com/api/v4/projects/46/repository/files/install_cockpit.sh/raw?ref=master')
+    case "$response" in
+        200) success "Download Success" && INSTALL ;;
+        301) information $response ;;
+        #304) printf "Received: HTTP $response (file unchanged) ==> $url\n" ;;
+        401) error "Not Authorized" && exit 1;;
+        404) error "file not found" && exit 1;;
+          *) information  "Received: HTTP $response " ;;
+    esac
+}
+
+function ENTER_TOKEN {
+    #cecho -c 'BrightGreen' "############################";
+    PRINT_HASH
+    read -p $'\e[0;37mEnter your Token    :\e[1;33m ' TOKEN
+    PRINT_HASH
+    #echo $'\e[0;92m############################'
+    cecho -c 'BrightWhite' "Token       =    ${TOKEN}"
+    PRINT_HASH
+    #cecho -c 'BrightGreen' "############################"
+    read -p $'\e[0;31mConfirm (Y) change Token (C) Exit (N)? (Y/N/C): \e[1;33m ' confirm
+    case $confirm in
+        y|Y )
+            DL_INSTALLER $TOKEN;;
+        c|C )
+            ENTER_TOKEN ;;   
+        * )
+            echo "Exit without installing"
+            echo $'\e[0m'
+            exit
+            ;;
+    esac
+
+}
+
+function PROCEED {
+    PRINT_HASH
+    #echo $'\e[0;92m############################';
+    echo ""
+    echo "to proceed you need a valid Customer ID and Token!"
+    echo ""
+    echo "Report any errors to support@c4sam.com"
+    echo ""
+    echo "Proceed with installation?"
+    echo ""
+
+    read -p $'\e[0;31mConfirm (Y) Exit (N)? (Y/N): \e[1;33m ' confirm
+    case $confirm in
+        y|Y )
+            ENTER_TOKEN ;;
+        * )
+            echo "Exit without installing"
+            echo $'\e[0m'
+            exit
+            ;;
+    esac
+
+}
+PRINT_HASH
+information "Hostname: ${_FQDN}" 
+information "Operating System: ${_OS} , Version: ${_OS_VER}" 
+information "Default Interface: ${_DEFAULT_INT}, Default GW: ${_DEFAULT_GW}"
+# get public IP
+myWanIP=`dig +short myip.opendns.com @resolver1.opendns.com`
+#echo "Public IP is ${myWanIP}"
+information "Public IP is ${myWanIP}"
+PRINT_HASH
+information "Checking Name Resolution"
+# Checking for the resolved IP address from the end of the command output. Refer
+# the normal command output of nslookup to understand why.
+resolvedIP=$(nslookup "${_REPOSITORY}" | awk -F':' '/^Address: / { matched = 1 } matched { print $2}' | xargs)
+
+# Deciding the lookup status by checking the variable has a valid IP string
+[[ -z "$resolvedIP" ]] && error "${_REPOSITORY}" lookup failure || success "${_REPOSITORY} resolved to ${resolvedIP}"
+
+#traceroute $resolvedIP
+
+#ping -c1 -W1 -q $resolvedIP && echo "${resolvedIP} is reachable" || echo "${resolvedIP} is down"
+PRINT_HASH
+
 CHECK_REPOSITORY_ALIVE $resolvedIP
+
+PRINT_HASH
 
 information "Checking Software Packages"
 
@@ -219,12 +295,14 @@ for i in $_REQUIREMENTS; do
     CHECK_PKG $i
 done 
 
+PRINT_HASH
+
 information "Checking Ports"
 for i in $_PORTS; do
     #echo "Check Port ${i} on ${_REPOSITORY}"
     CHECK_PORT $i
 done
-
+PRINT_HASH
 if [[ $_REQ_PORTS == "true" ]] ; then
     success "required ports open"
 else
@@ -239,10 +317,14 @@ else
     exit 1
 fi
 
-#if [[ $_OS_VER -le $_REQ_OS_VER ]] || [[ $_OS != "${_REQ_OS}" ]] ; then
-if [[ $_OS != "${_REQ_OS}" ]] ; then
-    error "Operating System ${_OS} ${_OS_VER} not supported, ${_REQ_OS} ${_REQ_OS_VER} required exiting."
-else
+if [[ $_OS_VER -ge $_REQ_OS_VER ]] && [[ $_OS == "${_REQ_OS}" ]] ; then
     success "Operating System supported"
+else
+    error "Operating System ${_OS} ${_OS_VER} not supported, ${_REQ_OS} ${_REQ_OS_VER} required, exiting."
     exit 1
 fi
+
+PROCEED
+
+
+exit 0
